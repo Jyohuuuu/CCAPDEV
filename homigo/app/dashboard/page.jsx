8071537d98a9587a8ca166a665a39b6dfe008625
Kian Carl Daylag <kian_daylag@dlsu.edu.ph>
2025-03-19 21:15:10 +0800

    made global font, redirects to user page first

--- homigo/app/dashboard/page.jsx
+++ homigo/app/dashboard/page.jsx
@@ -1,8 +1,9 @@
 // Import Necessary Modules/Components
-import PlaceholderPage from "@/components/placeholder_info";
+import PlaceholderPage from "@/components/placeholder_info";  
+import UserProfilePage from "@/components/user_page";
 
 //export makes it so that the function/etc can be imported into other files
 //Default makes the specified function the default export of the file
 export default function Dashboard() {
-  return <PlaceholderPage />;
+  return <UserProfilePage />;
 }
