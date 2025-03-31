a5d518e76aa47b25d9da3d50ec46ecd8fc83cc35
Jyohuuuu <157949020+Jyohuuuu@users.noreply.github.com>
2025-03-31 13:08:09 +0800

    Update mylistings.jsx
    fixed error code

--- homigo/components/mylistings.jsx
+++ homigo/components/mylistings.jsx
@@ -3,7 +3,7 @@
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import Header from "../components/Header";
+import Header from "../components/header";
 
 export default function MyListings() {
   const { data: session, status } = useSession();
