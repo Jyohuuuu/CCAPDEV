0d4f8f2f4fd697ed9d020bc8200c74972f634c67
Jyohuuuu <157949020+Jyohuuuu@users.noreply.github.com>
2025-04-04 11:40:22 +0800

    Update next.config.mjs

--- homigo/next.config.mjs
+++ homigo/next.config.mjs
@@ -1,11 +1,17 @@
-import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __dirname equivalent for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     domains: ["res.cloudinary.com"],
-    unoptimized: true, // Required for static exports
+    unoptimized: true
   },
   webpack: (config) => {
     config.resolve.alias = {
